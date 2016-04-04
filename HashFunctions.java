95e3cecf3d897f1ac9543a8c2a1e4f7172bba794
shadownater <shadownater@hotmail.com>
2016-04-04 14:12:10 -0600

    Hash functions in progress

--- HashFunctions.java
+++ HashFunctions.java
@@ -2,19 +2,57 @@ import java.util.*;
 import com.sleepycat.db.*;
 import java.io.*;
 import java.io.File.*;
-                     
+
 
 //this class will handle the cases of using the hash
 public class HashFunctions{
 
+  long beforeTime, afterTime;
+  
+//this function uses the hash table setup to search for a given key
   public void findByKey(String input){
     System.out.println("Find by key haha!");
+
+    
+    DatabaseEntry keyValue = new DatabaseEntry(input.getBytes());
+    keyValue.setSize(input.length());
+    
+    DatabaseEntry data = new DatabaseEntry();
+    
+    try{
+
+      Cursor cursor = Globals.my_table.openCursor(null, null);
+
+      
+      beforeTime = System.nanoTime();
+      OperationStatus opSts = cursor.getSearchKey(keyValue, data, null);
+
+      //if success - found something
+      if(opSts == OperationStatus.SUCCESS){
+      //get time stamp
+        afterTime = System.nanoTime();
+
+      //do this 4 times with different values each time!
+      //then record the average
+      //put the retrieved key/data pairs in a location called answers
+      //SEE ASSIGNMENT for the layout of that file
+      }else{
+      //if failure - didn't find anything
+        System.out.println("Unable to find given key.");
+      }
+      
+    }catch(DatabaseException dbe){
+      System.err.println("Search by key: "+dbe.toString());
+      System.exit(1);
+    }
+    System.out.println(beforeTime);
   }
 
   public void findByData(String input){
     System.out.println("Find by data haha!");
   }
 
+//finds: low <= x <= upp
   public void findByRange(String lowInput, String upperInput){
     System.out.println("Find by range haha!");
   }
